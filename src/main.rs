d6bbfa4e3762d1629ed3107cc98d4ac37fc7d316
Tommy <43862943+tommyp6@users.noreply.github.com>
2022-08-22 18:09:09 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,1 @@
-extern crate sanitize_filename;
-
-mod common;
-mod receiver;
-mod sender;
-
-pub fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    match args.get(1).unwrap_or(&String::new()).as_ref() {
-        "send" => match args.get(2) {
-            Some(file_path) => sender::run(file_path.into()),
-            None => println!("The sender needs a 'file path'"),
-        },
-        "recv" => receiver::run(),
-        _ => println!("Usage: recv | (send <filepath>)"),
-    }
-}
+fn main() {}
